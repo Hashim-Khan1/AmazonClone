2d74c190b9a928587c8d692c63e24b8926186645
fzippy <hk2011khan@gmail.com>
2023-04-02 15:16:08 +0100

    Made the amazon nav

---
+++ src/App.tsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const lowerNavArray = [
+    "Best Sellers",
+    "Gift Ideas",
+    "New Releases",
+    "Customer Service",
+    "Prime Video",
+    "Today's Deals",
+    "Music",
+    "Prime",
+    "Books",
+    "Audible",
+    "PC & Video Games",
+    "Vouchers",
+    "Kindle Books",
+    "PC",
+  ];
+  return (
+    <div className="App">
+      <nav>
+        <div className="row" id="upperNav">
+          <img src="src/assets/img/logo.png" id="logo" />
+          <div className="row upperNavContentBox" style={{ margin: "0 25px" }}>
+            <img src="src/assets/img/pin.png" id="pin" />
+            <div className="column">
+              <p className="greyText">Hello</p>
+              <p className="whiteBold" style={{ letterSpacing: "1px" }}>
+                Select your address
+              </p>
+            </div>
+          </div>
+          <div
+            className="row"
+            id="parentSearch"
+            style={{ alignItems: "center" }}
+          >
+            <input type="text" id="search" placeholder="Search Amazone Clone" />
+            <div className="searchBtn"></div>
+          </div>
+          <div className="row upperNavContentBox" style={{ margin: "0 25px" }}>
+            <img src="src/assets/img/gbFlag.png" id="flag" />
+            <div className="column">
+              <p className="greyText">Hello, sign up</p>
+              <p className="whiteBold" style={{ letterSpacing: "1px" }}>
+                Accounts & Lists
+              </p>
+            </div>
+          </div>
+          <div className="row upperNavContentBox" style={{ margin: "0 5px" }}>
+            <div className="column">
+              <p className="greyText">Returns</p>
+              <p className="whiteBold" style={{ letterSpacing: "1px" }}>
+                & Orders
+              </p>
+            </div>
+          </div>
+          <div
+            className="row upperNavContentBox"
+            style={{ margin: "0 20px", alignItems: "center" }}
+          >
+            <div className="column">
+              <p
+                style={{
+                  marginLeft: "10px",
+                  color: "#febd69",
+                  fontWeight: "bold",
+                }}
+              >
+                0
+              </p>
+              <img src="src/assets/img/basket.png" id="basket" />
+            </div>
+            <p
+              className="whiteBold"
+              style={{ letterSpacing: "1px", paddingLeft: "3px" }}
+            >
+              Basket
+            </p>
+          </div>
+        </div>
+        <div className="row" id="lowerNav">
+          {lowerNavArray.map((el: any) => {
+            return (
+              <>
+                <p>{el}</p>
+              </>
+            );
+          })}
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;

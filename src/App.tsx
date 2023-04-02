8f2517da57a6d94c01f2d7c7009008394641e817
fzippy <hk2011khan@gmail.com>
2023-04-02 19:23:47 +0100

    Made cards to display products and fixed home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Nav from "./components/Nav";
 
@@ -10,6 +8,84 @@ function App() {
   return (
     <div className="App">
       <Nav></Nav>
+      <div id="backgroundImg"></div>
+
+      <section id="firstSection">
+        <div className="row" style={{ justifyContent: "space-evenly" }}>
+          <div className="contentCardContainer column">
+            <p className="contentCardTitle">Pick up where you left off</p>
+            <div className="row">
+              <div className="contentBox column">
+                <img src="src/assets/img/weights.png" className="contentImg" />
+                <p>JLL® Cast Iron Weight P…</p>
+              </div>
+              <div className="contentBox column">
+                <img src="src/assets/img/weights.png" className="contentImg" />
+                <p>JLL® Cast Iron Weight P…</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="contentBox column">
+                <img src="src/assets/img/weights.png" className="contentImg" />
+                <p>JLL® Cast Iron Weight P…</p>
+              </div>
+              <div className="contentBox column">
+                <img src="src/assets/img/weights.png" className="contentImg" />
+                <p>JLL® Cast Iron Weight P…</p>
+              </div>
+            </div>
+          </div>
+          <div className="contentCardContainer column">
+            <p className="contentCardTitle">Keep shopping for</p>
+            <div className="row">
+              <div className="contentBox column">
+                <img src="src/assets/img/weights.png" className="contentImg" />
+                <p>JLL® Cast Iron Weight P…</p>
+              </div>
+              <div className="contentBox column">
+                <img src="src/assets/img/weights.png" className="contentImg" />
+                <p>JLL® Cast Iron Weight P…</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="contentBox column">
+                <img src="src/assets/img/weights.png" className="contentImg" />
+                <p>JLL® Cast Iron Weight P…</p>
+              </div>
+              <div className="contentBox column">
+                <img src="src/assets/img/weights.png" className="contentImg" />
+                <p>JLL® Cast Iron Weight P…</p>
+              </div>
+            </div>
+          </div>
+          <div className="contentCardContainer column">
+            <p className="contentCardTitle">Top Deal</p>
+            <div className="row">
+              <div className="contentBox column">
+                <img src="src/assets/img/weights.png" className="contentImg" />
+                <p>JLL® Cast Iron Weight P…</p>
+              </div>
+              <div className="contentBox column">
+                <img src="src/assets/img/weights.png" className="contentImg" />
+                <p>JLL® Cast Iron Weight P…</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="contentBox column">
+                <img src="src/assets/img/weights.png" className="contentImg" />
+                <p>JLL® Cast Iron Weight P…</p>
+              </div>
+              <div className="contentBox column">
+                <img src="src/assets/img/weights.png" className="contentImg" />
+                <p>JLL® Cast Iron Weight P…</p>
+              </div>
+            </div>
+          </div>
+          <div className="contentCardContainer column">
+            <p className="contentCardTitle">Today's Deals</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }

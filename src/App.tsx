db53b9f79068035541b0ae1df04efcff045f29d6
fzippy <hk2011khan@gmail.com>
2023-04-03 10:19:45 +0100

    Removed duplicate

--- src/App.tsx
+++ src/App.tsx
@@ -5,38 +5,6 @@ import FirstSection from "./layouts/FirstSection";
 
 function App() {
   const [count, setCount] = useState(0);
-  const weightsObject = [
-    {
-      productID: 0,
-      title: "JLL® Cast Iron Weight P…",
-      img: "src/assets/img/weights.png",
-    },
-    {
-      productID: 1,
-      title: "HLL® Cast Iron Weight P…",
-      img: "src/assets/img/weights.png",
-    },
-    {
-      productID: 2,
-      title: "DLL® Cast Iron Weight P…",
-      img: "src/assets/img/weights.png",
-    },
-    {
-      productID: 3,
-      title: "ZLL® Cast Iron Weight P…",
-      img: "src/assets/img/weights.png",
-    },
-  ];
-  const loadData = (objData: any) => {
-    return objData.map((obj: any) => {
-      return (
-        <div className="contentBox column" key={obj.productID}>
-          <img src={obj.img} className="contentImg" />
-          <p>{obj.title}</p>
-        </div>
-      );
-    });
-  };
   return (
     <div className="App">
       <Nav></Nav>

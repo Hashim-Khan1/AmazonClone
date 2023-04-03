950713a20f4933c4bbd966e098dd9dbc295d8ea3
fzippy <hk2011khan@gmail.com>
2023-04-03 11:26:24 +0100

    Obeject literal function

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,58 @@ import FirstSection from "./layouts/FirstSection";
 
 function App() {
   const [count, setCount] = useState(0);
+  const shoes = [
+    {
+      productID: 0,
+      title: "Sports shoe",
+      img: "src/assets/img/weights.png",
+    },
+    {
+      productID: 1,
+      title: "Sports shoe",
+      img: "src/assets/img/weights.png",
+    },
+    {
+      productID: 2,
+      title: "DLSports shoe",
+      img: "src/assets/img/weights.png",
+    },
+    {
+      productID: 3,
+      title: "Sports shoe",
+      img: "src/assets/img/weights.png",
+    },
+    {
+      productID: 4,
+      title: "Sports shoe",
+      img: "src/assets/img/weights.png",
+    },
+    {
+      productID: 5,
+      title: "Sports shoe",
+      img: "src/assets/img/weights.png",
+    },
+    {
+      productID: 6,
+      title: "DLSports shoe",
+      img: "src/assets/img/weights.png",
+    },
+    {
+      productID: 7,
+      title: "Sports shoe",
+      img: "src/assets/img/weights.png",
+    },
+  ];
+  const loadData = (objData: any) => {
+    return objData.map((obj: any) => {
+      return (
+        <div className="imgAndText" key={obj.productID}>
+          <div className="circleImg"></div>
+          <p>{obj.title}</p>
+        </div>
+      );
+    });
+  };
   return (
     <div className="App">
       <Nav></Nav>
@@ -13,40 +65,7 @@ function App() {
       <section>
         <div className="longScrollContainer">
           <p className="contentCardTitle">Pick up where you left off</p>
-          <div className="row">
-            <div className="imgAndText">
-              <div className="circleImg"></div>
-              <p>title</p>
-            </div>
-            <div className="imgAndText">
-              <div className="circleImg"></div>
-              <p>title</p>
-            </div>
-            <div className="imgAndText">
-              <div className="circleImg"></div>
-              <p>title</p>
-            </div>
-            <div className="imgAndText">
-              <div className="circleImg"></div>
-              <p>title</p>
-            </div>
-            <div className="imgAndText">
-              <div className="circleImg"></div>
-              <p>title</p>
-            </div>
-            <div className="imgAndText">
-              <div className="circleImg"></div>
-              <p>title</p>
-            </div>
-            <div className="imgAndText">
-              <div className="circleImg"></div>
-              <p>title</p>
-            </div>
-            <div className="imgAndText">
-              <div className="circleImg"></div>
-              <p>title</p>
-            </div>
-          </div>
+          <div className="row">{loadData(shoes)}</div>
         </div>
       </section>
     </div>

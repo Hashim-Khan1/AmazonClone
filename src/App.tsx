0867450f80d6a8e165b9e841da3062d4adad1dae
fzippy <hk2011khan@gmail.com>
2023-04-03 20:26:40 +0100

    Created sign in

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,42 @@ function App() {
 
   return (
     <div className="App">
+      {/*        
+      Home Page
       <Nav></Nav>
       <div id="backgroundImg"></div>
       <FirstSection></FirstSection>
       <SecondSection></SecondSection>
       <ThirdSection></ThirdSection>
-      <Footer></Footer>
+      <Footer></Footer>*/}
+      <div id="container">
+        <div id="loginLogo"></div>
+        <div className="formLoginContainer">
+          <p id="formTitle">Sign in</p>
+          <form className="column" action="#">
+            <p className="label">Email or mobile phone number</p>
+            <input className="formInpt" type="text" />
+            <input type="submit" className="loginBtn" value={"Continue"} />
+          </form>
+        </div>
+        <div
+          className="row alignItemsCenter"
+          style={{ width: "350px", margin: "20px auto" }}
+        >
+          <div className="line"></div>
+          <p
+            style={{
+              minWidth: "max-content",
+              fontSize: "13px",
+              margin: "0 15px",
+            }}
+          >
+            New to Not amazon
+          </p>
+          <div className="line"></div>
+        </div>
+        <div className="greyGBtn">Create your not amazon account</div>
+      </div>
     </div>
   );
 }

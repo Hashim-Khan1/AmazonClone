53605c8be975de20950b628966d41506aaec1021
fzippy <hk2011khan@gmail.com>
2023-04-06 12:00:34 +0100

    Made a respone component for when you sign in

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -3,8 +3,15 @@ import Register from "../layouts/Register";
 function SignInPage() {
   return (
     <div id="container">
+      <div id="response">
+        <p id="repsoneTitle">Reponse message</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+          totam sunt officiis ab tenetur voluptas.
+        </p>
+      </div>
       <SignIn></SignIn>
-      <Register></Register>
+      {/* <Register></Register> */}
     </div>
   );
 }

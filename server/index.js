41860a9b9bfbbc65ade7b456fdbed04a508bf212
fzippy <hk2011khan@gmail.com>
2023-04-06 18:48:28 +0100

    Added env and body parser dependancy

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+require("dotenv").config();
+
+const routes = require("./api/index");
+routes(app, { urlencodedParser });
+const PORT = 3000;
 
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Server is running on: ${PORT} `);
+});

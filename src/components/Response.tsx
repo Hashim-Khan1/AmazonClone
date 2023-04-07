9d2ced113f181f4d9e8c880a47e633d191f8350c
fzippy <hk2011khan@gmail.com>
2023-04-07 19:48:54 +0100

    Added props

--- src/components/Response.tsx
+++ src/components/Response.tsx
@@ -1,11 +1,8 @@
-function Response() {
+function Response(props: any) {
   return (
-    <div id="response">
-      <p id="repsoneTitle">Reponse message</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam totam
-        sunt officiis ab tenetur voluptas.
-      </p>
+    <div id="response" style={{ backgroundColor: `${props.colour}` }}>
+      <p id="repsoneTitle">{props.title}</p>
+      <p>{props.message}</p>
     </div>
   );
 }

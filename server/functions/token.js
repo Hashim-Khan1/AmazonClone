8eab18d13a7e3ad9f81fb33b9c7be6fef6420868
fzippy <hk2011khan@gmail.com>
2023-04-07 21:04:26 +0100

    Create JWT token and verify token

---
+++ server/functions/token.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
+
+const createJWTToken = function (username) {
+  const token = jwt.sign({ id: username }, process.env.ACCESS_TOKEN, {
+    expiresIn: "30d",
+  });
+  return token;
+};
+const verifyJWTToken = function (value) {
+  const verify = jwt.verify(value, process.env.ACCESS_TOKEN, (err, data) => {
+    if (err) return err;
+    return data;
+  });
+  return verify;
+};
+module.exports = {};

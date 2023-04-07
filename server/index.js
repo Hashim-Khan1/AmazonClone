3e86777084e3d28abac1dcee2cf7ef33b7588dc4
fzippy <hk2011khan@gmail.com>
2023-04-07 18:22:50 +0100

    Set up cors

--- server/index.js
+++ server/index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
+const cors = require("cors");
 require("dotenv").config();
 
 const routes = require("./api/index");
+const corsOptions = {
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 routes(app, { urlencodedParser });
 const PORT = 3000;
 

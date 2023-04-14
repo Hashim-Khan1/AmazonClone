12bd3783e7af042e570a164e47520ba8986423ef
fzippy <hk2011khan@gmail.com>
2023-04-14 17:40:43 +0100

    Fixed pagenation clientside

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -6,26 +6,53 @@ import Footer from "../components/Footer";
 
 function ProductPage() {
   const [pageOffset, setpageOffset] = useState(1);
+  const [pageLength, setpageLength] = useState();
   const [productData, setProductData] = useState([]);
 
   const loadProductData = async () => {
     let urlPath = window.location.pathname.split("/");
     const lastIndex = urlPath.slice(-1)[0];
-    console.log(lastIndex);
     try {
       const res = await axios.post("http://localhost:3000/post/load-products", {
         category: lastIndex,
         limit: pageOffset,
       });
       setProductData(res.data.products);
-      console.log(res.data.products.length);
     } catch (error) {
       console.log(error);
     }
   };
+
+  const loadRows = async () => {
+    let urlPath = window.location.pathname.split("/");
+    const lastIndex = urlPath.slice(-1)[0];
+    console.log(lastIndex);
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/post/load-products-length",
+        {
+          category: lastIndex,
+        }
+      );
+      setpageLength(res.data.productsLength);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const renderPageNation = (pageNumbers: any) => {
+    const numberOfRows = Math.ceil(pageNumbers / 4);
+    const array = Array.from({ length: numberOfRows }, (_, index) => index + 1);
+    return array.map((element) => {
+      return (
+        <div className="pageNationRow row">
+          <div onClick={clickFunction}>{element}</div>
+        </div>
+      );
+    });
+  };
+
   const renderProducts = () => {
     const today = new Date();
-
     const date = today.getDate();
     const monthNames = [
       "January",
@@ -42,7 +69,6 @@ function ProductPage() {
       "December",
     ];
 
-    const d = new Date();
     return productData.map((element) => {
       return (
         <a
@@ -56,7 +82,7 @@ function ProductPage() {
             <p className="productTitleProductPage">{element.productTitle}</p>
             <p className="productTitleProductPage">{element.price}</p>
             <p>
-              Get it by {date + 1} {monthNames[d.getMonth()]}
+              Get it by {date + 1} {monthNames[today.getMonth()]}
             </p>
             <p>FREE Delivery by Amazon </p>
           </div>
@@ -64,12 +90,15 @@ function ProductPage() {
       );
     });
   };
+
   const clickFunction = (e: any) => {
-    console.log(e.currentTarget.innerText);
+    setpageOffset(e.currentTarget.innerText);
   };
+
   useEffect(() => {
     loadProductData();
-  }, []);
+    loadRows();
+  }, [pageOffset]);
   return (
     <>
       <Nav></Nav>
@@ -83,12 +112,12 @@ function ProductPage() {
       >
         <section style={{ backgroundColor: "white", minHeight: "100vh" }}>
           {productData != false ? renderProducts() : "no products "}
-          <div className="pageNationRow row">
-            <div onClick={clickFunction}>1</div>
-            <div onClick={clickFunction}>2</div>
-            <div onClick={clickFunction}>3</div>
-          </div>
         </section>
+        <div className="pageNationRow row">
+          {productData != false ? renderPageNation(pageLength) : "no products "}
+        </div>
+
+        {}
       </div>
       <Footer></Footer>
     </>

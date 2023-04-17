341c5a0c0a035a2c187282e882d519bc93e2541e
fzippy <hk2011khan@gmail.com>
2023-04-18 00:11:53 +0100

    Made second session parrallel with nav

--- src/layouts/SecondSection.tsx
+++ src/layouts/SecondSection.tsx
@@ -1,68 +1,20 @@
 import { useState } from "react";
 
-function SecondSection() {
+function SecondSection(props: any) {
   const [count, setCount] = useState(0);
-  const shoes = [
-    {
-      productID: 0,
-      title: "Sports shoe",
-      img: "src/assets/img/weights.png",
-      category: "mensShoes",
-    },
-    {
-      productID: 1,
-      title: "Sports shoe",
-      img: "src/assets/img/weights.png",
-      category: "Womenshoes",
-    },
-    {
-      productID: 2,
-      title: "DLSports shoe",
-      img: "src/assets/img/weights.png",
-      category: "mensShoes",
-    },
-    {
-      productID: 3,
-      title: "Sports shoe",
-      img: "src/assets/img/weights.png",
-      category: "mensShoes",
-    },
-    {
-      productID: 4,
-      title: "Sports shoe",
-      img: "src/assets/img/weights.png",
-      category: "mensShoes",
-    },
-    {
-      productID: 5,
-      title: "Sports shoe",
-      img: "src/assets/img/weights.png",
-      category: "mensShoes",
-    },
-    {
-      productID: 6,
-      title: "DLSports shoe",
-      img: "src/assets/img/weights.png",
-      category: "mensShoes",
-    },
-    {
-      productID: 7,
-      title: "Sports shoe",
-      img: "src/assets/img/weights.png",
-      category: "mensShoes",
-    },
-  ];
+  const shoes = props.navItems;
+
   const loadData = (objData: any) => {
     return objData.map((obj: any) => {
       return (
         <a
-          href={`products/${obj.category}`}
+          href={`products/${obj}`}
           className="imgAndText"
           key={obj.productID}
           style={{ textDecoration: "none", color: "#000000" }}
         >
           <div className="circleImg borderRnd"></div>
-          <p className="productTitle">{obj.title}</p>
+          <p className="productTitle">{obj}</p>
         </a>
       );
     });
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,13 +3,28 @@ import FirstSection from "../layouts/FirstSection";
 import SecondSection from "../layouts/SecondSection";
 import ThirdSection from "../layouts/ThirdSection";
 import Footer from "../components/Footer";
+import { useState } from "react";
 function HomePage() {
+  const [navItems, setNavItems] = useState();
+  const lowerNavArray = [
+    "Sports",
+    "Fragrances",
+    "Mobile phones",
+    "Tablets",
+    "TV",
+    "Consoles",
+    "Food",
+    "Books",
+    "Audio devices",
+    "Video Games",
+    "PC",
+  ];
   return (
     <>
       <Nav></Nav>
       <div id="backgroundImg"></div>
       <FirstSection></FirstSection>
-      <SecondSection></SecondSection>
+      <SecondSection navItems={lowerNavArray}></SecondSection>
       <ThirdSection></ThirdSection>
 
       <Footer></Footer>
